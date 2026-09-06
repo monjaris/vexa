2c25829eda17c2480b28006aeca2fba00f93a7a2
Monjaris <200353384+Monjaris@users.noreply.github.com>
2026-09-06 22:12:52 +0400

    rename: vendor directory

--- vendor/SDL3/src/core/linux/SDL_extra_stubs.c
+++
@@ -1,5 +0,0 @@
-#include "SDL_internal.h"
-
-const char *SDL_GetAppID(void) { return "cone"; }
-const char *SDL_GetExeName(void) { return "cone"; }
-int SDL_IOReady(int fd, int flags, Sint64 timeoutNS) { return 0; }
--- vendor/SDL3/src/core/linux/SDL_gtk_stubs.c
+++
@@ -1,6 +0,0 @@
-#include "SDL_internal.h"
-
-void SDL_UpdateGtk(void) {}
-void SDL_Gtk_Quit(void) {}
-void SDL_Gtk_EnterContext(void) {}
-void SDL_Gtk_ExitContext(void) {}
